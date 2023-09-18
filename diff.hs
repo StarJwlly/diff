37ac4a9ca1c825796248081e3f49a61d22f188e3
a <a>
2023-09-18 08:56:13 -0300

    add getdiff

--- diff.hs
+++ diff.hs
@@ -6,19 +6,40 @@ import System.IO
 import Data.Ratio
 
 main = do
-  (path : _) <- getArgs
-  h <- openFile path ReadMode
-  hSetEncoding h utf8
-  contents <- hGetContents h
-  let timingsLine = snd $ Prelude.break ("[TimingPoints]"==) $ Data.List.lines contents
+  (mapPath : diffPath : _) <- getArgs
+  mapFile <- openFile mapPath ReadMode
+  diffFile <- openFile diffPath ReadMode
+  hSetEncoding mapFile utf8
+  hSetEncoding diffFile utf8
+  mapContents <- hGetContents mapFile
+  diffContents <- hGetContents diffFile
+
+  let timingsLine = snd $ Prelude.break ("[TimingPoints]"==) $ Data.List.lines mapContents
   let timingPoints = getTimingPoints timingsLine
   let notesLine = tail $ snd $ Prelude.break ("[HitObjects]"==) timingsLine
+  let chords = readHitObjects notesLine 10
+  let diffFunctionArgs = getDiffFunctionArgs 0 $ Data.List.lines diffContents
+  let dists = getDistances chords 0 (Data.List.replicate 10 (-1, -1))
+  
+  putStrLn $ show $ getDiff diffFunctionArgs chords dists 0
+  
+  
   --putStrLn $ show $ getTimingPoints timingsLine
   --putStrLn $ show $ readHitObjects notesLine 10
-  putStrLn $ show $ getDistances (readHitObjects notesLine 10) 0 $ Data.List.replicate 10 (-1, -1)
-  
-  
 
+getDiffFunctionArgs :: Int -> [String] -> [[(Double, Double, Double)]]
+getDiffFunctionArgs _ [] = []
+getDiffFunctionArgs i (s:ss) = curr : (getDiffFunctionArgs (i + 1) ss)
+  where curr = Data.List.map (\x -> let temp = Data.List.map read $ Data.List.words x :: [Double] in (temp !! 0, temp !! 1, temp !! 2)) $ splitOnDelimiter s "|"
+
+riceFunc :: Double -> Double -> Double -> Int -> Int
+riceFunc a b c x = round (-x1 * x1 * a + x1 * b + c)
+  where x1 = fromIntegral x
+
+jackFunc :: Double -> Double -> Double -> Int -> Int
+jackFunc a b c x = round (-a * (logBase b (c * x1)))
+  where x1 = fromIntegral x
+  
 splitOnDelimiter :: String -> String -> [String]
 splitOnDelimiter s d = Data.List.map (unpack) $ splitOn (pack d) $ pack s
 
@@ -82,10 +103,34 @@ toChord hitObjects releases tim = let r = unzip result in (Chord tim (fst r), sn
         
 
 
-getDistances :: [Chord] -> Int -> [(Int, Int)] -> [[(Int, Int)]]
+getDistances :: [Chord] -> Int -> [(Int, Int)] -> [(Int, [(Int, Int)])]
 getDistances [] _ _ = []
-getDistances ((Chord time notes) : next) i lasts = lasts : rest
+getDistances ((Chord time notes) : next) i lasts = (i, lasts) : rest
   where rest = (getDistances next (i + 1)　$ Data.List.map (\(x, (ya, yb)) -> case x of
                                                                                Nothing       -> (ya, yb)
                                                                                Just HoldNote -> (ya, yb)
                                                                                Just n        -> (i, ya)) (Data.List.zip notes lasts))
+
+
+getDiff :: [[(Double, Double, Double)]] -> [Chord] -> [(Int, [(Int, Int)])] -> Int -> [[[Int]]]
+getDiff _ _ [] _ = []
+getDiff args chords ((curr, di):sts) threshold = (Data.List.map (\x -> case x of
+                                                                            Just y -> diffs y
+                                                                            Nothing -> []) getNotes) : getDiff args chords sts threshold
+  where currChord = chords !! curr
+        getNotes = fst $ Data.List.foldl (\(x, c) y -> case y of
+          Just ReleaseNote -> (x ++ [Just c], c + 1)
+          Just HitNote -> (x ++ [Just c], c + 1)
+          otherwise -> (x ++ [Nothing], c + 1)
+            ) ([], 0) $ notes currChord 
+        diffs c1 = let r = Data.List.foldl (\(x, c) (y, ys) -> if y < 0 then (x ++ [0], c + 1)
+                                                                        else if c == c1
+                                                                               then (x ++ [applyJack (args !! c1 !! c) $ distms y], c + 1)
+                                                                               else let d = distms y in
+                                                                                 if d >= threshold then (x ++ [applyRice (args !! c1 !! c) d], c + 1)
+                                                                                                   else if ys < 0 then (x ++ [0], c + 1)
+                                                                                                                  else (x ++ [applyRice (args !! c1 !! c) $ distms ys], c + 1)
+                                                                                                                    ) ([], 0) di in Data.List.map (max 0) (fst r) 
+        applyJack (a, b, c) x = jackFunc a b c x
+        applyRice (a, b, c) x = riceFunc a b c x
+        distms ind = (time $ chords !! curr) - (time $ chords !! ind)
